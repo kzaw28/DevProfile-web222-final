e2de9f33ee2076601063322806f8c88b9f153070
Kaung Khant Zaw <113069280+kzaw28@users.noreply.github.com>
2022-12-05 23:31:34 -0500

    Add files via upload

---
+++ js/edu.js
@@ -0,0 +1,23 @@
+/* 
+
+edu.js
+
+It lists an array of objects, describing school name, degree name and
+a short description
+
+*/
+
+window.education = [
+  {
+    school: "Seneca College",
+    degree: "Advanced Diploma in Computer Programming & Analysis",
+    year: "2022 - 2024",
+    description: "Learned a lot about coding and achieved amazing achievements"
+  },
+  {
+    school: "Seneca College",
+    degree: "Advanced Diploma in Computer Programming & Analysis",
+    year: "2022 - 2024",
+    description: "Learned a lot about coding and achieved amazing achievements"
+  }
+];
